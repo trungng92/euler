ef479377a89037d1efc5eaa469c82fe60f7817f9
Trung Nguyen <trungng92@gmail.com>
2016-10-30 19:11:03 -0400

    Solved: Create function that will manually create the name.

--- p17.py
+++ p17.py
@@ -1,43 +1,76 @@
+# If the numbers 1 to 5 are written out in words: one, two, three, four, five, then there are 3 + 3 + 5 + 4 + 4 = 19 letters used in total.
+# If all the numbers from 1 to 1000 (one thousand) inclusive were written out in words, how many letters would be used?
+# NOTE: Do not count spaces or hyphens. For example, 342 (three hundred and forty-two) contains 23 letters and 115 (one hundred and fifteen) contains 20 letters. The use of "and" when writing out numbers is in compliance with British usage.
+
+
 # The first 19 numbers are unique
-first_nine = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+# Have a blank string for zero because we don't say twenty zero, we say twenty
+# and it makes strings convenient because index 1 is "one", index 9 is "nine", etc
+first_nine = ["", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
 # ten_teens contains all the weird numbers between 10 and 19 (inclusive)
 ten_teens = ["ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"]
 # Each 10s is unique too
-tens = ["twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"]
-# After those, 20 through 29 are the same as 90-99
+# Skip 0 and 10 so that indices are easier to work with
+tens = ["", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"]
 
-# slight inefficiency since this has to create a whole list just to sum them
-def get_length_strings(array):
-	return sum([len(num) for num in array])
+#determines whether or not we want spaces when forming the number string (this is only for readability)
+space = ""
+# This only takes numbers up to 1000
+def num_to_str(num):
+	num_str = ""
+	while num > 0:
+		if num == 1000:
+			return "one" + space + "thousand"
+		elif num < 1000 and num >= 100:
+			hundreds_digit = num // 100
+			num_str += first_nine[hundreds_digit] + space + "hundred" + space
+			num %= 100
+			# we have to put "and" when we have a number that isn't divisible by 100
+			if num != 0:
+				num_str += "and" + space
+		# numbers below 20 are unique, so handle those cases separately
+		elif num < 100 and num >= 20:
+			tens_digit = num // 10
+			num_str += tens[tens_digit] + space
+			num %= 10
+		elif num < 20 and num >= 10:
+			ones_digit = num % 10
+			num_str += ten_teens[ones_digit]
+			num = 0
+		elif num < 10:
+			num_str += first_nine[num]
+			num = 0
+	return num_str
 
-# Length from 1-9
-first_nine_length = get_length_strings(first_nine)
-print("length first nine", first_nine_length)
+# Test cases
+print("Printing test cases:")
+print(1, num_to_str(1))
+print(5, num_to_str(5))
+print(10, num_to_str(10))
+print(15, num_to_str(15))
+print(19, num_to_str(19))
+print(20, num_to_str(20))
+print(25, num_to_str(25))
+print(99, num_to_str(99))
+print(100, num_to_str(100))
+print(170, num_to_str(170))
+print(171, num_to_str(171))
+print(900, num_to_str(900))
+print(999, num_to_str(999))
+print(1000, num_to_str(1000))
 
-# 20-99, we just find the length of the tens and then multiply it by 10 because each level of 10 occurs 10 times
-# i.e. twenty, twenty one ... twenty nine
-tens_length = get_length_strings(tens) * 10
-# And then we need to find the second digit, which is just the same as the first nine for each tens
-tens_length += first_nine_length * len(tens)
+# Result
+print("\n\nCalculating result:")
+space = ""
+answer = sum([len(num_to_str(num)) for num in range(1001)])
+print("Answer is", answer)
 
-# Now take into account the length from 10-19
-ten_teens_length = get_length_strings(ten_teens) 
-tens_length += ten_teens_length
-print("length tens", tens_length)
-
-# 100-999
-# first digit (and don't forget about "and")
-# find number of "hundreds" there are from 100-999 inclusive
-# one hundred, one hundred and one ... one hundred and ninety nine
-# Note that "and" doesn't appear for the first entry
-hundreds_length = sum([len("hundred") + len(num) * 100 for num in first_nine]) + len("and") * (100 - 1) * 9
-# hundreds_length = first_nine_length + len("hundred") + len("and") * (999 - 100 + 1)
-hundreds_length += tens_length + first_nine_length * 9
-print("length hundreds", hundreds_length)
-
-thousands_length = len("onethousand")
-
-print("length thousands", thousands_length)
-
-total_length = first_nine_length + tens_length + hundreds_length + thousands_length
-print("length total up to one thousand", total_length)
+# If you want to see the actual output
+show_output = False
+if show_output:
+	agg_len = 0
+	for num in range(1001):
+		current_str = num_to_str(num)
+		current_len = len(current_str)
+		ans += current_len
+		print("length of\t", current_str, "\t\t", current_len, "aggregate length\t", agg_len)
